327fc3004ba25f5c554b0acc24e7db75eb3e0138
Sergey Surganov <surganov@me.com>
2014-08-18 16:53:32 +0400

    1.22 & 1.24 in Scheme

--- 1.2 Procedures and the Processes They Generate/1-22.scm
+++ 1.2 Procedures and the Processes They Generate/1-22.scm
@@ -8,7 +8,7 @@
 (define (runtime) (current-inexact-milliseconds))
 
 (define (timed-prime-test n)
-   (start-prime-test n (runtime)))
+    (start-prime-test n (runtime)))
 
 (define (start-prime-test n start-time)
     (if (prime? n)
@@ -25,21 +25,18 @@
 
 
 (define (smallest-divisor n)
-   (find-divisor n 2))
+    (find-divisor n 2))
 
 (define (find-divisor n test-divisor)
-   (cond ((> (square test-divisor) n) n)
+    (cond ((> (square test-divisor) n) n)
          ((divides? test-divisor n) test-divisor)
          (else (find-divisor n (+ test-divisor 1)))))
 
-(define (divides? a b)
-   (= (remainder b a) 0))
+(define (divides? a b) (= (remainder b a) 0))
 
-(define (square x)
-   (* x x))
+(define (square x) (* x x))
 
-(define (prime? n)
-   (= n (smallest-divisor n)))
+(define (prime? n) (= n (smallest-divisor n)))
 
 ; Using this procedure, write a procedure search-for-primes that checks the primality of consecutive odd integers in a specified range. Use your procedure to find the three smallest primes larger than 1000; larger than 10,000; larger than 100,000; larger than 1,000,000. Note the time needed to test each prime. Since the testing algorithm has order of growth of Θ(√n), you should expect that testing for primes around 10,000 should take about √10 times as long as testing for primes around 1000. Do your timing data bear this out? How well do the data for 100,000 and 1,000,000 support the Θ(√n) prediction? Is your result compatible with the notion that programs on your machine run in time proportional to the number of steps required for the computation?
 
@@ -52,9 +49,25 @@
               ((timed-prime-test range-start) (search-for-primes (+ range-start 2) range-end (- count 1)))
               (else (search-for-primes (+ range-start 2) range-end count)))))
 
+
 ; Ratios stay very close to √10. These results do seem to verify that programs run in time proportional to the number of steps required for the computation.
 
 (search-for-primes 1000    10000    3)
+; 1009 *** 0.0009765625
+; 1013 *** 0.0009765625
+; 1019 *** 0.0009765625
+
 (search-for-primes 10000   100000   3)
+; 10007 *** 0.0009765625
+; 10009 *** 0.001953125
+; 10037 *** 0.001953125
+
 (search-for-primes 100000  1000000  3)
+; 100003 *** 0.00390625
+; 100019 *** 0.00390625
+; 100043 *** 0.004150390625
+
 (search-for-primes 1000000 10000000 3)
+; 1000003 *** 0.013916015625
+; 1000033 *** 0.013916015625
+; 1000037 *** 0.013916015625
---
+++ 1.2 Procedures and the Processes They Generate/1-24.scm
@@ -0,0 +1,70 @@
+; Exercise 1.24
+; Modify the timed-prime-test procedure of Exercise 1.22 to use fast-prime? (the Fermat method), and test each of the 12 primes you found in that exercise. Since the Fermat test has Θ(log(n)) growth, how would you expect the time to test primes near 1,000,000 to compare with the time needed to test primes near 1000? Do your data bear this out? Can you explain any discrepancy you find?
+
+#lang scheme
+
+
+(define (runtime) (current-inexact-milliseconds))
+
+(define (timed-prime-test n)
+   (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+    (if (fast-prime? n 10000)
+        (report-prime (- (runtime) start-time) n)
+        false))
+
+(define (report-prime elapsed-time n)
+    (display n)
+    (display " *** ")
+    (display elapsed-time)
+    (newline))
+
+
+
+(define (square x) (* x x))
+
+(define (expmod base exp m)
+    (cond ((= exp 0) 1)
+            ((even? exp)
+             (remainder
+             (square (expmod base (/ exp 2) m))
+              m))
+        (else
+            (remainder
+            (* base (expmod base (- exp 1) m))
+             m))))
+
+(define (fermat-test n)
+    (define (try-it a) (= (expmod a n n) a))
+    (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+    (cond ((= times 0) true)
+          ((fermat-test n) (fast-prime? n (- times 1)))
+           (else false)))
+
+
+(for-each timed-prime-test '(1009 1013 1019))
+; 1009 *** 5.365966796875
+; 1013 *** 5.404052734375
+; 1019 *** 6.06201171875
+
+(for-each timed-prime-test '(10007 10009 10037))
+; 10007 *** 6.552001953125
+; 10009 *** 6.300048828125
+; 10037 *** 6.662109375
+
+(for-each timed-prime-test '(100003 100019 100043))
+; 100003 *** 7.908935546875
+; 100019 *** 8.197021484375
+; 100043 *** 8.787841796875
+
+(for-each timed-prime-test '(1000003 1000033 1000037))
+; 1000003 *** 9.087158203125
+; 1000033 *** 8.68505859375
+; 1000037 *** 9.48193359375
+
+
+; This supports the theory of logarithmic growth:
+; log(1000037) / log(1009) ≈ 9.48193359375 / 5.365966796875 ≈ 2 times
